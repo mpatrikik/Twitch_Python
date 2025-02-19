8358aa64a14c342c068af5a5461db36da1c17cab
mpatrikik <m.patrik01@gmail.com>
2025-02-19 15:38:14 +0100

    async main definied, debugging needed

--- Twitch.py
+++ Twitch.py
@@ -7,6 +7,7 @@ import psutil
 from dotenv import load_dotenv
 import tkinter as tk
 from tkinter import messagebox
+import asyncio
 
 load_dotenv()
 
@@ -29,55 +30,58 @@ def close_app(app_name):
                 print(f"{app_name} closed.")
             except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
                 pass
-try:
-    twitch = Twitch(CLIENT_ID, CLIENT_SECRET)
-    twitch.authenticate_app([])
-    auth = UserAuthenticator(twitch, USER_SCOPE, force_verify=False, url=REDIRECT_URI)
-    token, refresh_token = auth.authenticate()
-    twitch.set_user_authentication(token, USER_SCOPE, refresh_token)
-except Exception as e:
-    print(f"Error during Twitch API authentication: {e}")
-    exit()
 
 
-channel_name = input("Please add the channel name: ")
-
-try:
-    stream = twitch.get_streams(user_login=channel_name)
-    if stream['data']:
-        print(f"The {channel_name} is streaming now.")
-        stream = True
-    else:
-        print(f"The {channel_name} is not streaming now.")
-        stream = False
-except Exception as e:
-    print(f"Error while querying channel status: {e}")
-    exit()
-
-
-while streaming:
+async def main():
     try:
-        time.sleep(60)
-        stream = twitch.get_streams(user_login=[channel_name])
-
-        if not stream['data']:
-            print("The stream ended!")
-            streaming = False
-            close_app("chrome.exe")
-
-            root = tk.Tk()
-            root.withdraw()
-
-            result = messagebox.askyesno("End of stream", "Want to shut down the PC?")
-
-            if result:
-                print("Shutting down...")
-                os.system("shutdown /s /t 5")
-            else:
-                print("Script ends.")
-            root.destroy()
-            break
+        twitch = Twitch(CLIENT_ID, CLIENT_SECRET)
+        await twitch.authenticate_app([])
+        auth = UserAuthenticator(twitch, USER_SCOPE, force_verify=False, url=REDIRECT_URI)
+        token, refresh_token = await auth.authenticate()
+        twitch.set_user_authentication(token, USER_SCOPE, refresh_token)
     except Exception as e:
-        print(f"Error while checking: {e}")
+        print(f"Error during Twitch API authentication: {e}")
+        exit()
+
+    channel_name = input("Please add the channel name: ")
+
+    try:
+        stream = await twitch.get_streams(user_login=channel_name)
+        if stream['data']:
+            print(f"The {channel_name} is streaming now.")
+            stream = True
+        else:
+            print(f"The {channel_name} is not streaming now.")
+            stream = False
+    except Exception as e:
+        print(f"Error while querying channel status: {e}")
+        exit()
 
 
+    while streaming:
+        try:
+            time.sleep(60)
+            stream = await twitch.get_streams(user_login=[channel_name])
+
+            if not stream['data']:
+                print("The stream ended!")
+                streaming = False
+                close_app("chrome.exe")
+
+                root = tk.Tk()
+                root.withdraw()
+
+                result = messagebox.askyesno("End of stream", "Want to shut down the PC?")
+
+                if result:
+                    print("Shutting down...")
+                    os.system("shutdown /s /t 5")
+                else:
+                    print("Script ends.")
+                root.destroy()
+                break
+        except Exception as e:
+            print(f"Error while checking: {e}")
+
+if __name__ == '__main__':
+    asyncio.run(main())
