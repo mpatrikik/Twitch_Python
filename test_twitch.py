ada3e81546aa84db959d8d32f7e4e62b3614bed3
mpatrikik <m.patrik01@gmail.com>
2025-02-23 14:53:17 +0100

    mocking authenticaion run, but debug needed

--- test_twitch.py
+++ test_twitch.py
@@ -5,23 +5,34 @@ from Twitch import main
 
 class TestTwitch(unittest.IsolatedAsyncioTestCase):
 
-    @patch('twitch.close_app')
-    @patch('twitchAPI.Twitch.get_streams', new_callable=AsyncMock)
+    @patch('Twitch.close_app')
+    @patch('Twitch.Twitch.get_streams', new_callable=AsyncMock)
     async def test_stream_end(self, mock_get_streams, mock_close_app):
-        mock_get_streams.return_value = []
+        async def async_iterable():
+            yield []
+
+        mock_get_streams.return_value.__aiter__ = async_iterable
         with patch('builtins.input', return_value='test_channel'):
             await main()
+
         mock_get_streams.assert_called()
         mock_close_app.assert_called_with('chrome.exe')
 
-    @patch('twitch.close_app')
-    @patch('twitchAPI.Twitch.get_streams', new_callable=AsyncMock)
+
+
+
+    @patch('Twitch.close_app')
+    @patch('Twitch.Twitch.get_streams', new_callable=AsyncMock)
     async def test_stream_start(self, mock_get_streams, mock_close_app):
-        mock_get_streams.return_value = [{'user_login': 'test_channel', 'type': 'live'}]
+        async def async_iterable():
+            yield [{'user_login': 'test_channel', 'type': 'live'}]
+
+        mock_get_streams.return_value.__aiter__ = async_iterable
         with patch('builtins.input', return_value='test_channel'):
             await main()
+
         mock_get_streams.assert_called()
-        mock_close_app.assert_not_called()
+        mock_close_app.assert_called_with('chrome.exe')
 
 
 if __name__ == '__main__':
